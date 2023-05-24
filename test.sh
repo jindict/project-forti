1681b3507a9208107afcfe2fa40f282c52459ebf
Ginn <claire@ginnMac.local>
2023-05-24 15:15:22 +0800

    new one

---
+++ test.sh
@@ -0,0 +1,4 @@
+curl -k -X POST https://172.16.22.253:56443/api/v2/monitor/system/config/backup-H "Content-Type: application/json" -d '{"access_token": "5tpbQ3kQdmbGznN7jkk6Gc6t66zj8H", "scope": "global", "vdom": "root"}'
+
+
+5tpbQ3kQdmbGznN7jkk6Gc6t66zj8H
